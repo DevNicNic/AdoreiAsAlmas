de2747785e991f1b7673c75549e007ffbb6fc0fd
Nicole Moraes <nicatrombone@gmail.com>
2026-01-09 10:17:07 -0300

    adding comments

--- app/src/main/java/com/nicnicdev/adoreiasalmas/login/LoginScreen.kt
+++ app/src/main/java/com/nicnicdev/adoreiasalmas/login/LoginScreen.kt
@@ -19,70 +19,79 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+
+//Função Tela de Login
 @Composable
 fun LoginScreen(
+    // aqui a tela recebe o ViewModel responsavel pela lógica do login
     loginViewModel: LoginViewModel = viewModel()
 ) {
+    //aqui pegamos o estado atual da tela (email, senha, loading, erro )
+    //sempre que o estado mudar a tela atualiza sozinha
     val state by loginViewModel.state.collectAsState()
 
+    // essa coluna organiza os elemntos um em baixo do outro
     Column(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(24.dp),
-        verticalArrangement = Arrangement.Center
+            .fillMaxSize() // ocupa a tela inteira
+            .padding(24.dp), // da um espacinho nas bordas
+        verticalArrangement = Arrangement.Center //centraliza tudo verticalmente
     ) {
-
+        // Título da tela
         Text(
             text = "Login",
             style = MaterialTheme.typography.headlineMedium
         )
-
+        // espaço entre os elementos
         Spacer(modifier = Modifier.height(16.dp))
 
+        // campo onde o usuário digita o email
         OutlinedTextField(
-            value = state.email,
-            onValueChange = { loginViewModel.onEmailChange(it) },
+            value = state.email, // mostra o email que está salvo no estado
+            onValueChange = { loginViewModel.onEmailChange(it) }, // salva o que a pessoa digitar
             label = { Text("Email") },
             modifier = Modifier.fillMaxWidth(),
             singleLine = true
 
         )
-
         Spacer(modifier = Modifier.height(8.dp))
 
+        // campo onde o usuário digita a senha
         OutlinedTextField(
-            value = state.password,
-            onValueChange = { loginViewModel.onPasswordChange(it) },
+            value = state.password, //mostra a senha salva no estado
+            onValueChange = { loginViewModel.onPasswordChange(it) }, // atualiza a senha no ViewModel
             label = { Text("Senha") },
             modifier = Modifier.fillMaxWidth(),
             singleLine = true
-
         )
-
         Spacer(modifier = Modifier.height(16.dp))
 
+        // Se existir alguma mensagem de erro , ela aparece aqui
+        // Se não tiver erro, esse bloco  não aparece
         state.errorMessage?.let { errorMessage ->
 
             Text(
                 text = errorMessage,
-                color = MaterialTheme.colorScheme.error
+                color = MaterialTheme.colorScheme.error // cor vermelha de erro
             )
 
             Spacer(modifier = Modifier.height(8.dp))
         }
-
+        // botão entrar
         Button(
-            onClick = { loginViewModel.onLoginClick() },
+            onClick = { loginViewModel.onLoginClick() },// chama a lógica de login
             modifier = Modifier.fillMaxWidth(),
-            enabled = !state.isLoading
+            enabled = !state.isLoading // desativa o botão enquanto esté carregando
 
         ) {
+            // se estiver carregando, mostra um loading
             if (state.isLoading) {
                 CircularProgressIndicator(
                     modifier = Modifier.size(20.dp),
                     strokeWidth = 2.dp
                 )
             } else {
+                // Se não estiver carregando , mostra o texto normal
                 Text("Entrar")
             }
         }
