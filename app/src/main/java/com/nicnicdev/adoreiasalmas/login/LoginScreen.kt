d2721b385fdd90c6c5c74e51308d6bc16ac53720
Nicole Moraes <nicatrombone@gmail.com>
2026-01-12 22:59:16 -0300

    refactor: reorganize login screen layout

--- app/src/main/java/com/nicnicdev/adoreiasalmas/login/LoginScreen.kt
+++ app/src/main/java/com/nicnicdev/adoreiasalmas/login/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.nicnicdev.adoreiasalmas.login
-
+import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -22,11 +23,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.nicnicdev.adoreiasalmas.R
 
 //Função Tela de Login
 @Composable
@@ -38,78 +42,138 @@ fun LoginScreen(
     //sempre que o estado mudar a tela atualiza sozinha
     val state by loginViewModel.state.collectAsState()
 
+    Box(
+        modifier = Modifier.fillMaxSize()
+    )  {
+        Image(
+            painter =  painterResource(id = R.drawable.conselhopretovelho_split_41),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop)
 
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black.copy(alpha = 0.4f))
+        )
 
-    // essa coluna organiza os elemntos um em baixo do outro
-    Column(
-        modifier = Modifier
-            .fillMaxSize() // ocupa a tela inteira
-            .padding(24.dp)
-
-    ) {
+        // essa coluna organiza os elemntos um em baixo do outro
         Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+                .fillMaxSize() // ocupa a tela inteira
+                .padding(24.dp)
+
         ) {
-            // Título da tela
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                // Título da tela
+                Text(
+                    text = "Acesse sua Conta",
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center
+                )
+                // espaço entre os elementos
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // campo onde o usuário digita o email
+                OutlinedTextField(
+                    value = state.email, // mostra o email que está salvo no estado
+                    onValueChange = { loginViewModel.onEmailChange(it) }, // salva o que a pessoa digitar
+                    label = { Text("Email") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(56.dp), // deixa o campo mais alto e confortavel
+                    singleLine = true,
+                    shape = MaterialTheme.shapes.large // arredonda bem as laterais
+
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+
+                // campo onde o usuário digita a senha
+                OutlinedTextField(
+                    value = state.password, //mostra a senha salva no estado
+                    onValueChange = { loginViewModel.onPasswordChange(it) }, // atualiza a senha no ViewModel
+                    label = { Text("Senha") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(56.dp),
+                    singleLine = true,
+                    shape = MaterialTheme.shapes.large
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // Se existir alguma mensagem de erro , ela aparece aqui
+                // Se não tiver erro, esse bloco  não aparece
+                state.errorMessage?.let { errorMessage ->
+
+                    Text(
+                        text = errorMessage,
+                        color = MaterialTheme.colorScheme.error // cor vermelha de erro
+                    )
+
+                    Spacer(modifier = Modifier.height(8.dp))
+                }
+                Box(
+                    modifier = Modifier.fillMaxWidth(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    // botão entrar
+                    Button(
+                        onClick = { loginViewModel.onLoginClick() },// chama a lógica de login
+                        enabled = !state.isLoading, // desativa o botão enquanto esté carregando
+                        contentPadding = PaddingValues(
+                            vertical = 8.dp,
+                            horizontal = 20.dp
+                        ),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color(0xFF6D4C41).copy(alpha = 0.85f),
+                            contentColor = Color.White
+                        )
+                    )
+                    {
+                        // se estiver carregando, mostra um loading
+                        if (state.isLoading) {
+                            CircularProgressIndicator(
+                                modifier = Modifier.size(20.dp),
+                                strokeWidth = 2.dp
+                            )
+                        } else {
+                            // Se não estiver carregando , mostra o texto normal
+                            Text("Entrar")
+                        }
+                    }
+                }
+
+            }
+
+            Spacer(modifier = Modifier.height(6.dp))
+
+            // texto para usuário recuperar a senha (ainda sem ação)
             Text(
-                text = "Acesse sua Conta",
-                style = MaterialTheme.typography.titleLarge,
+                text = "Esqueci a Senha",
+                color = MaterialTheme.colorScheme.primary,
+                fontSize = 14.sp,
                 modifier = Modifier.fillMaxWidth(),
+                style = MaterialTheme.typography.bodyMedium,
                 textAlign = TextAlign.Center
             )
-            // espaço entre os elementos
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(6.dp))
 
-            // campo onde o usuário digita o email
-            OutlinedTextField(
-                value = state.email, // mostra o email que está salvo no estado
-                onValueChange = { loginViewModel.onEmailChange(it) }, // salva o que a pessoa digitar
-                label = { Text("Email") },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(56.dp), // deixa o campo mais alto e confortavel
-                singleLine = true,
-                shape = MaterialTheme.shapes.large // arredonda bem as laterais
-
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-
-            // campo onde o usuário digita a senha
-            OutlinedTextField(
-                value = state.password, //mostra a senha salva no estado
-                onValueChange = { loginViewModel.onPasswordChange(it) }, // atualiza a senha no ViewModel
-                label = { Text("Senha") },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(56.dp),
-                singleLine = true,
-                shape = MaterialTheme.shapes.large
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-
-            // Se existir alguma mensagem de erro , ela aparece aqui
-            // Se não tiver erro, esse bloco  não aparece
-            state.errorMessage?.let { errorMessage ->
-
-                Text(
-                    text = errorMessage,
-                    color = MaterialTheme.colorScheme.error // cor vermelha de erro
-                )
-
-                Spacer(modifier = Modifier.height(8.dp))
-            }
             Box(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 24.dp),
                 contentAlignment = Alignment.Center
             ) {
-                // botão entrar
+                //botão para levar o usuário a tela de cadastro
                 Button(
-                    onClick = { loginViewModel.onLoginClick() },// chama a lógica de login
-                    enabled = !state.isLoading, // desativa o botão enquanto esté carregando
+                    onClick = { /*TODO*/ },
                     contentPadding = PaddingValues(
                         vertical = 8.dp,
                         horizontal = 20.dp
@@ -118,60 +182,16 @@ fun LoginScreen(
                         containerColor = Color(0xFF6D4C41).copy(alpha = 0.85f),
                         contentColor = Color.White
                     )
-                )
-                {
-                    // se estiver carregando, mostra um loading
-                    if (state.isLoading) {
-                        CircularProgressIndicator(
-                            modifier = Modifier.size(20.dp),
-                            strokeWidth = 2.dp
-                        )
-                    } else {
-                        // Se não estiver carregando , mostra o texto normal
-                        Text("Entrar")
-                    }
+                ) {
+                    Text("Cadastrar")
+
                 }
-            }
-
-        }
-
-        Spacer(modifier = Modifier.height(6.dp))
-
-        // texto para usuário recuperar a senha (ainda sem ação)
-        Text(
-            text = "Esqueci a Senha",
-            color = MaterialTheme.colorScheme.primary,
-            fontSize = 14.sp,
-            modifier = Modifier.fillMaxWidth(),
-            style = MaterialTheme.typography.bodyMedium,
-            textAlign = TextAlign.Center
-        )
-        Spacer(modifier = Modifier.height(6.dp))
-
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 24.dp),
-            contentAlignment = Alignment.Center
-        ) {
-            //botão para levar o usuário a tela de cadastro
-            Button(
-                onClick = { /*TODO*/ },
-                contentPadding = PaddingValues(
-                    vertical = 8.dp,
-                    horizontal = 20.dp
-                ),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color(0xFF6D4C41).copy(alpha = 0.85f),
-                    contentColor = Color.White
-                )
-            ) {
-                Text("Cadastrar")
-
             }
         }
     }
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
